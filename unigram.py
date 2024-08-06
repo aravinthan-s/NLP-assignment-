f32bc2f2615cdd63eef4e48488062ddf6e6c5f88
aravinthan-s <sec22ad047@sairamtap.edu.in>
2024-08-06 14:07:28 +0530

    Update unigram.py

--- unigram.py
+++ unigram.py
@@ -2,15 +2,9 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from collections import Counter
-
 nltk.download('punkt')
-
 text = "the cat sat on the mat the cat is happy"
-
-# Tokenize the text
 tokens = word_tokenize(text.lower())
-
-# Count unigrams
 unigrams = Counter(tokens)
 
 print("Unigrams:")
